71b30ec75e47557d9491bfafcddb058d81f6c8b8
Felix <felix.wedler@t-online.de>
2025-11-06 15:16:23 +0100

    Implemented basic functionality

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cerr << "There is no path to a config file. Use the command like this:\n\t\t authority <PATH TO CONFIG>" << std::endl;
+        exit(1);
+    } else if (argc > 2) {
+        std::cout << "More than the expected amount of arguments given. Taking " << argv[1] << "as the config path" << std::endl;
+    }
+
+    std::string configFile = argv[1];
+    std::vector<std::string> lines;
+
+    std::ifstream reader(configFile);
+
+    if (reader.rdstate() == std::fstream::failbit) {
+        std::cerr << "Encountered an error when attempting to read the file. Is the path correct?" << std::endl;
+    }
+
+    int lineCounter = 0;
+    std::string tmp;
+    while(getline(reader, tmp)) {
+        lines.push_back(tmp);
+        tmp = "";
+        lineCounter++;
+    }
+
+    reader.close();
+    if (reader.rdstate() == std::fstream::failbit) {
+        std::cerr << "Encountered an error when attempting to close the file." << std::endl;
+    }
+
+    std::cout << "Read " << lineCounter << " lines" << std::endl;
+
+    if (lineCounter > 0) {
+        std::mt19937 rng(clock());
+        uint32_t selected = static_cast<float>(rng()) / static_cast<float>(rng.max()) * lineCounter;
+
+        std::transform(lines[selected].begin(), lines[selected].end(), lines[selected].begin(), [](auto c) {return std::toupper(c);});
+        std::cout << "YOU SHOULD DO: \n\n\t\t" << lines[selected] << std::endl << std::endl;
+    }
+}
